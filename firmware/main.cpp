ff3865f1a8aaec16579272a1f324de2e4a00144f
Matthew Kennedy <matthewkennedy@outlook.com>
2023-08-15 18:43:41 -0700

    status LEDs go blinky

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,6 +1,9 @@
 #include "ch.h"
 #include "hal.h"
 
+#define LEFT_LED_LINE PAL_LINE(GPIOA, 15)
+#define RIGHT_LED_LINE PAL_LINE(GPIOB, 2)
+
 static const CANConfig canConfig500 =
 {
     CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
@@ -10,13 +13,56 @@ static const CANConfig canConfig500 =
     CAN_BTR_SJW(0) | CAN_BTR_BRP(5)  | CAN_BTR_TS1(12) | CAN_BTR_TS2(1),
 };
 
-int main(void) {
+static void setLeftStatusLed(bool state)
+{
+    if (state)
+    {
+        palSetLine(LEFT_LED_LINE);
+    }
+    else
+    {
+        palClearLine(LEFT_LED_LINE);
+    }
+}
+
+static void setRightStatusLed(bool state)
+{
+    if (state)
+    {
+        palSetLine(RIGHT_LED_LINE);
+    }
+    else
+    {
+        palClearLine(RIGHT_LED_LINE);
+    }
+}
+
+static void initStatusLeds()
+{
+    setLeftStatusLed(false);
+    setRightStatusLed(false);
+
+    palSetLineMode(LEFT_LED_LINE, PAL_MODE_OUTPUT_PUSHPULL);
+    palSetLineMode(RIGHT_LED_LINE, PAL_MODE_OUTPUT_PUSHPULL);
+}
+
+int main(void)
+{
     halInit();
     chSysInit();
+
+    initStatusLeds();
 
     canStart(&CAND1, &canConfig500);
 
     while (true) {
-        chThdSleepMilliseconds(1);
+        setLeftStatusLed(true);
+        chThdSleepMilliseconds(100);
+        setLeftStatusLed(false);
+        chThdSleepMilliseconds(100);
+        setRightStatusLed(true);
+        chThdSleepMilliseconds(100);
+        setRightStatusLed(false);
+        chThdSleepMilliseconds(100);
     }
 }
